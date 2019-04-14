e714f5afd925b61d463d8ed83a0f68478bae8b62
Hassan Nazar <hnazar@redbridge.se>
2019-04-15 00:21:04 +0200

    Start for a simple distributed coffeshop application

---
+++ micro-openliberty/traced-liberty/CoffeShop/src/main/java/io/codelair/coffeshop/shop/resource/JaxRsApplication.java
@@ -0,0 +1,8 @@
+package io.codelair.coffeshop.shop.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class JaxRsApplication extends Application {
+}
